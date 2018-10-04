4de1e763e727aa490bf882fdc750b92b2ce2e650
Luis Grijalva <grijalvarmero@gmail.com>
2018-10-03 19:27:36 -0600

    Primer avance
    haciendo una prueba

---
+++ CompiladorRecetas/src/sample/Main.java
@@ -0,0 +1,41 @@
+package sample;
+
+import com.sun.javafx.application.LauncherImpl;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+    public  static int duracion=10000;
+    public  static int steps=1;
+
+    @Override
+    public void init() throws Exception {
+        for(int i=0;i<duracion;i++){
+            double progress=(100*i)/duracion;
+            LauncherImpl.notifyPreloader(this,
+                    new Splash.ProgressNotification(progress));
+            System.out.println((100*i)/duracion);
+        }//llave for
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("principal.fxml"));
+        primaryStage.setTitle("FoodCompiler");
+        primaryStage.setScene(new Scene(root, 960, 600));
+      //  primaryStage.initStyle(StageStyle.TRANSPARENT);
+
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        LauncherImpl.launchApplication(Main.class,Splash.class,args);
+        //launch(args);
+    }
+}
---
+++ CompiladorRecetas/src/sample/Splash.java
@@ -0,0 +1,69 @@
+package sample;
+
+import javafx.application.Platform;
+import javafx.application.Preloader;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+public class Splash extends Preloader {
+    private Label lblProgress;
+    private ProgressBar barra;
+    private Stage stage;
+    private Scene scene;
+
+    @Override
+    public void handleStateChangeNotification(StateChangeNotification info) {
+        StateChangeNotification.Type type=info.getType();
+        switch (type){
+            case BEFORE_START:{
+                stage.hide();
+                break;
+            }
+        }//llave switch
+
+    }
+
+    @Override
+    public void handleApplicationNotification(PreloaderNotification info) {
+        if(info instanceof ProgressNotification){
+            lblProgress.setText( ((ProgressNotification)info).getProgress()+"%");
+            barra.setProgress( ((ProgressNotification)info).getProgress()/ 100);
+        }//llave iFFFFFFFFFFFFFFFFFF
+    }
+
+    @Override
+    public void init() throws Exception {
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                Parent root2;
+                try{
+                    root2= FXMLLoader.load(getClass().getResource("splash.fxml"));
+                    scene=new Scene(root2,500,500);
+                }catch (IOException e){
+                    e.printStackTrace();
+                }//llave CATch
+            }
+        });
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        this.stage=stage;
+       // this.stage.initStyle(StageStyle.UNDECORATED);scene.setFill(Color.TRANSPARENT);
+        this.stage.setScene(scene);
+        this.stage.show();
+
+        lblProgress=(Label) scene.lookup("#lblProg");
+        barra=(ProgressBar) scene.lookup("#prog");
+
+    }
+}
